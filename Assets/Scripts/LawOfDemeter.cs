45afec8ce8d7452285b59ee8ca2e5cf7ba955e2f
Denis König <denis.koenig@edu.bib.de>
2022-10-10 17:20:34 +0200

    update law of demeter

---
+++ Assets/Scripts/LawOfDemeter.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LawOfDemeter : MonoBehaviour
+{
+    //reduce the dependancies of each component as much as possible
+}
